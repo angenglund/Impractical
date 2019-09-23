43a8ca4d03f177d6ccde6a664184362aee107e71
A. Rainbolt-Greene <angelaenglund@gmail.com>
2019-09-22 18:47:27 -0700

    renamed first file

--- 001pseudonyms.py
+++ 001pseudonyms.py
@@ -3,6 +3,7 @@ import sys
 import random
 
 # https://github.com/rlvaugh/Impractical_Python_Projects/commit/0b7be69b79f0b09e402757544c417e9851f4408d
+
 def main():
     """Choose names at random from 2 tuples of names and print to screen."""
     print("Welcome to the Psych 'Sidekick Name Picker. '\n")
@@ -49,11 +50,11 @@ def main():
         print("{} {}".format(firstName, lastName), file=sys.stderr)
         print("\n\n")
 
-        try_again = input("\n\nTry again? (Press Enter else n to quit)/n ")
+        try_again = input("\n\nTry again? (Press Enter else n to quit)")
         if try_again.lower() == 'n':
             break
 
-    input("\nPress Enter to exit.")
+    input('\nPress Enter to exit.')
 
 if __name__ == "__main__":
     main()
