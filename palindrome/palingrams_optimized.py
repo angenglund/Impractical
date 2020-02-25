940122aebea241a69eeb6ed7a98141c70d4e2163
A. Rainbolt-Greene <angelaenglund@gmail.com>
2020-02-24 17:58:59 -0800

    insert commit message here

---
+++ palindrome/palingrams_optimized.py
@@ -0,0 +1,12 @@
+def find_palingrams():
+    """Find dictionary palingrams."""
+    pali_list = []
+    words = set(word_list)
+    for word in words:
+        end = len(word)
+        rev_word = word[::-1]
+        if end > 1:
+            for i in range(end):
+                if word[i:] == rev_word[:end-i]and rev_word[end-i:]in words:
+                    pali_list.append((word, rev_word[end-i:]))
+                if word[:i] == rev_word((
