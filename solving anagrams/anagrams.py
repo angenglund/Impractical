480b46c65593c0df7f3be963639b716fc10652e0
A. Rainbolt-Greene <angelaenglund@gmail.com>
2020-03-04 14:15:06 -0800

    new anagrams project

---
+++ solving anagrams/anagrams.py
@@ -0,0 +1,36 @@
+# Load digital dictionary file as a list of words
+# Accept a word from user
+# Create an empty list to hold anagrams
+# Sort the user-word
+# Loop through each word in the word list:
+#     Sort the word
+#     if word sorted is equal to user-word sorted:
+#         Append word to anagrams list
+# Print anagrams list
+
+import load_dictonary
+
+word_list = load_dictonary.load('2of4brif.txt')
+
+anagram_list = []
+
+# input a SINGLE word or SINGLE name below to find its anagram(s):
+name = 'Foster'
+print("Input name = {}".format(name))
+name = name.lower()
+print("Using name = {}".format(name))
+
+# sort name & find anagrams
+name_sorted = sorted(name_sorted)
+for word in word_list:
+    word = word.lower()
+    if word != name:
+        if sorted(word) == name_sorted:
+            anagram_list.append(word)
+
+# print out list of anagrams
+print()
+if len(anagram_list) == 0:
+    print("You need a larger dictionary or a new name!")
+else:
+    print("Anagrams =", *anagram_list, sep='\n')
